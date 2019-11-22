b5fe2a30940c48e34d743d45e513cd9c221a9ad0
ernestpascual <eepascual@gmail.com>
2019-11-22 22:13:09 +0800

    initial commit

---
+++ app/src/main/java/com/blockdevs/blockdevs/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.blockdevs.blockdevs
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity() {
+    /**
+     * TANGEM X BLOCKDEVS WORKSHOP
+     * 
+     */
+
+    // TODO: Initialize SDK Functions to interact with the Tangem Cards
+
+
+    // TODO: Lazy initialization of string
+
+
+    // TODO: Get wallet address from publicKey
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // TODO: Interact with Android NFC using the tangem SDK
+
+
+        // TODO: Trigger scan card method on button click
+
+        // TODO: Sign card using sample hash or card's public key
+
+
+    }
+
+}
+
